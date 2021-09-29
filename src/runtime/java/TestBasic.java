87c1c3d5d90044574faef65670de10602d18e5fb
Arianna Masciolini <uzkamascio@gmail.com>
2021-09-29 09:47:12 +0200

    reintroduce all tests for newNGF (passing)

--- src/runtime/java/TestBasic.java
+++ src/runtime/java/TestBasic.java
@@ -184,19 +184,19 @@ public class TestBasic {
 
   // TODO: something like Python's test_readNGF
   
-  //@Test
-  //public void newNGF_File() throws FileAlreadyExistsException {
-  //  String path = "empty.pgf";
-  //  PGF pgf = PGF.newNGF("empty", path);
-  //  assertEquals(0, pgf.getCategories().size());
-  //  new File(path).delete();
-  //}
+  @Test
+  public void newNGF_File() throws FileAlreadyExistsException {
+    String path = "empty.ngf";
+    PGF pgf = PGF.newNGF("empty", path);
+    assertEquals(0, pgf.getCategories().size());
+    new File(path).delete();
+  }
 
-  //@Test
-  //public void newNGF_Memory() {
-  //  PGF pgf = PGF.newNGF("empty");
-  //  assertEquals(0, pgf.getCategories().size());
-  //}
+  @Test
+  public void newNGF_Memory() {
+    PGF pgf = PGF.newNGF("empty");
+    assertEquals(0, pgf.getCategories().size());
+  }
 
   @Test
   public void newNGF_ExixtingFile() throws FileAlreadyExistsException, FileNotFoundException {
