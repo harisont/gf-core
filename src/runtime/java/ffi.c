b314119e0e2a3ce892652d1c501d8a70db07911d
Arianna Masciolini <uzkamascio@gmail.com>
2021-10-05 12:40:47 +0200

    WIP unmarshaller for Types

--- src/runtime/java/ffi.c
+++ src/runtime/java/ffi.c
@@ -85,3 +85,48 @@ jstring2pgf_text(JNIEnv *env, jstring s)
 	(*env)->ReleaseStringUTFChars(env, s, text);
 	return pgfText;
 }
+
+// marshalling/unmarshalling
+
+JPGF_INTERNAL PgfType
+dtyp(JNIEnv *env, PgfUnmarshaller *this, int n_hypos, PgfTypeHypo *hypos, PgfText *cat, int n_exprs, PgfExpr *exprs)
+{
+	// construct empty list (TODO: handle failures?)
+	jclass lClass = (*env)->FindClass(env, "java/util/ArrayList");
+	jmethodID lConstr = (*env)->GetMethodID(env, lClass, "<init>", "()V");
+	jobject lHypos = (*env)->NewObject(env, lClass, lConstr);
+
+	// get id of the method to add elements to such list
+	jmethodID lAdd = (*env)->GetMethodID(env, lClass, "add", "(Ljava/lang/Object;)Z");
+
+	// get constructor id of Hypo class
+	jclass hClass = (*env)->FindClass(env, "org/grammaticalframework/pgf/Hypo");
+	jmethodID hConstr = (*env)->GetMethodID(env, hClass, "<init>", "(Ljava/lang/Object;J)V");		
+
+	for (int i = 0; i < n_hypos; i++) {
+		// get bindType, var and type from current Hypo
+		jboolean bindType = (jboolean)hypos[i].bind_type; // will this cast work? lo scopriremo solo vivendo
+		jstring var = pgf_text2jstring(env,hypos[i].cid); // and does this function actually work?
+		jobject type = (jobject)hypos[i].type; // but actually Type or jobject? mmm
+
+		// construct Hypo object
+		jobject hObj = (*env)->NewObject(env, hClass, hConstr, bindType, var, type);
+
+		// add it to the list of Hypos
+		(*env)->CallBooleanMethod(env, lHypos, lAdd, hObj);
+	}
+
+	// get cat as jstring
+	jstring cString = pgf_text2jstring(env,cat); 
+
+	jobject lExprs = (*env)->NewObject(env, lClass, lConstr);
+	// TODO: fill it with Exprs
+
+	// construct a Java Type object
+	jclass tClass = (*env)->FindClass(env, "org/grammaticalframework/pgf/Type");
+	jmethodID tConstr = (*env)->GetMethodID(env, tClass, "<init>", "()V");
+	jobject type = (*env)->NewObject(env, tClass, tConstr, lHypos, cString, lExprs);
+
+	// return it casted to PgfType 
+	return (PgfType) type ;
+}
